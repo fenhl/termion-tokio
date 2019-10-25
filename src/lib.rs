a53ce9602a49a9da7817ae678022abcaa7f2e33b
Fenhl <fenhl@fenhl.net>
2019-10-25 16:21:04 +0000

    Minimal error example

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,43 @@
-extern crate bytes;
-extern crate futures;
-extern crate termion;
-extern crate tokio;
+use std::{
+    io,
+    pin::Pin,
+    task::Context
+};
+use futures::{
+    Future,
+    Poll
+};
 
-pub mod input;
+enum ReadLineState<R> {
+    Error(io::Error),
+    Read(Vec<u8>, R),
+    Done,
+}
+
+struct ReadLineFuture<R>(ReadLineState<R>);
+
+impl<R> From<io::Error> for ReadLineFuture<R> {
+    fn from(error: io::Error) -> Self {
+        ReadLineFuture(ReadLineState::Error(error))
+    }
+}
+
+impl<R> ReadLineFuture<R> {
+    fn new(source: R) -> Self {
+        ReadLineFuture(ReadLineState::Read(Vec::with_capacity(30), source))
+    }
+}
+
+impl<R: tokio::io::AsyncRead> Future for ReadLineFuture<R> {
+    type Output = io::Result<(Option<String>, R)>;
+
+    fn poll(self: Pin<&mut Self>, ctx: &mut Context) -> Poll<Self::Output> {
+        match self.0 {
+            ReadLineState::Read(_, input) => {
+                let mut byte = [0x8, 1];
+                input.poll_read(ctx, &mut byte)
+            }
+            _ => unimplemented!()
+        }
+    }
+}
